09f9e804d8732ff4a67da5cdd702c07b617c4b5a
Vallz-vsc <thorfin919@gmail.com>
2025-05-28 17:13:44 +0700

    fixing counter dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,10 @@
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
-    // Jika belum login, redirect ke halaman login
     header("Location: login.php");
     exit();
 }
 
-// Cek apakah session counter sudah diset
 if (!isset($_SESSION['counter'])) {
     $_SESSION['counter'] = 1;
 } else {
